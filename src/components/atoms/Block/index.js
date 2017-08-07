bfd9938d71c5b40a68e6c84c74279778a2241363
Unknown <leksi4ek@gmail.com>
2017-08-07 15:27:02 +0300

    IE fix

--- src/components/atoms/Block/index.js
+++ src/components/atoms/Block/index.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 
 const Block = ({ children, className, tag, style, ...props }) => {
   const BlockElement = tag || 'div'
--- src/components/organisms/Map/index.js
+++ src/components/organisms/Map/index.js
@@ -1,4 +1,5 @@
-import React, { PropTypes, Component } from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { Block, Marker, InfoForm } from 'components'
 import GoogleMapReact from 'google-map-react'
 import config from 'config'
